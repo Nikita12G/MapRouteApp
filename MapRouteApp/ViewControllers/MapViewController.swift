45312c70b76fb47e25895c96c40fb2c1a503f8b4
Nikita12G <gulyi08@mail.ru>
2022-01-13 16:11:44 +0700

    Zooming in location and pin at map

--- MapRouteApp/ViewControllers/MapViewController.swift
+++ MapRouteApp/ViewControllers/MapViewController.swift
@@ -7,14 +7,24 @@
 
 import UIKit
 import MapKit
-
+import CoreLocation
 
 class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest // battery
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
     
     @IBAction func searchButton(_ sender: Any) {
@@ -29,8 +39,8 @@ class MapViewController: UIViewController {
 
 extension MapViewController: UISearchBarDelegate {
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
-    {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        
         //Ignoring user
         self.view.isUserInteractionEnabled = false
         
@@ -58,33 +68,60 @@ extension MapViewController: UISearchBarDelegate {
             activityIndicator.stopAnimating()
             self.view.isUserInteractionEnabled = false
             
-            if response == nil
-            {
+            if response == nil {
                 print("ERROR")
-            }
-            else
-            {
+            } else {
+                
                 //Remove annotations
                 let annotations = self.mapView.annotations
                 self.mapView.removeAnnotations(annotations)
                 
                 //Getting data
-                let latitude = response?.boundingRegion.center.latitude
-                let longitude = response?.boundingRegion.center.longitude
+                guard let latitude = response?.boundingRegion.center.latitude else { return }
+                guard let longitude = response?.boundingRegion.center.longitude else { return }
                 
                 //Create annotation
                 let annotation = MKPointAnnotation()
                 annotation.title = searchBar.text
-                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                annotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
                 self.mapView.addAnnotation(annotation)
                 
                 //Zooming in on annotation
-                let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
+                let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
                 let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
                 let region = MKCoordinateRegion(center: coordinate, span: span)
                 self.mapView.setRegion(region, animated: true)
             }
             
         }
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            
+            render(location)
+        }
+    }
+    
+    func render (_ location: CLLocation) {
+        
+        //Zooming in location
+        let coordinate = CLLocationCoordinate2D(
+            latitude: location.coordinate.latitude,
+            longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        // Pin at map
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
     }
 }
